618a60b68fb752a6a9332c53a90c32b9ab31553a
Marie Chatfield Rivas <marie.chatfield@gmail.com>
2023-12-02 14:36:25 -0600

    Update packages and install prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,32 @@
 /* eslint-disable no-undef */
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    plugins: [
-      '@typescript-eslint',
-    ],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-    ],
-    rules : {
-      "@typescript-eslint/explicit-member-accessibility": "off"
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  rules: {
+    "@typescript-eslint/explicit-member-accessibility": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
+  ignorePatterns: ["lib/external/**"],
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ["*.ts", "*.tsx"],
+      rules: {
+        "@typescript-eslint/explicit-member-accessibility": [
+          "error",
+          {
+            overrides: {
+              constructors: "no-public",
+            },
+          },
+        ],
+      },
     },
-    ignorePatterns: ["lib/external/**"],
-    overrides: [
-      {
-        // enable the rule specifically for TypeScript files
-        files: ["*.ts", "*.tsx"],
-        rules: {
-          "@typescript-eslint/explicit-member-accessibility": [
-            "error",
-            {
-              overrides: {
-                constructors: "no-public"
-              }
-            }
-          ]
-        }
-      }     
-    ]
-  };
+  ],
+};
