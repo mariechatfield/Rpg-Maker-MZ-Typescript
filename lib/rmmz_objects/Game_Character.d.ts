8cb229e5bb39aa488b7bde47b30c463acbd15b86
Marie Chatfield Rivas <marie.chatfield@gmail.com>
2023-12-02 23:50:54 -0600

    Game character too

--- lib/rmmz_objects/Game_Character.d.ts
+++ lib/rmmz_objects/Game_Character.d.ts
@@ -1,5 +1,28 @@
-import { Game_CharacterBase } from ".";
+import { Game_CharacterBase, Game_Item } from ".";
+import { RPG } from "../RPG";
+import { CharacterIndex, FaceIndex } from "../constants/sprites";
 
-declare class Game_Character extends Game_CharacterBase {}
+declare class Game_Character extends Game_CharacterBase {
+  public _actorId: 0;
+  public _name: string;
+  public _nickname: string;
+  public _classId: number;
+  public _level: number;
+  public _characterName: string;
+  public _characterIndex: CharacterIndex;
+  public _faceName: string;
+  public _faceIndex: FaceIndex;
+  public _battlerName: string;
+  public _exp: { [classId: number]: number };
+  public _skills: number[];
+  public _equips: Game_Item[];
+  public _actionInputIndex: number;
+  public _lastMenuSkill: Game_Item;
+  public _lastBattleSkill: Game_Item;
+  public _lastCommandSymbol: string;
+
+  public actorId(): number;
+  public actor(): RPG.DataActor;
+}
 
 export { Game_Character };
